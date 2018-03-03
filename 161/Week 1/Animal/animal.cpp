6dd372a9f4f0b6598847b1904be8d13ff1bbb8ad
Devin <gendron.devin@gmail.com>
2018-03-02 21:21:18 -0500

    Add files via upload

---
+++ 161/Week 1/Animal/animal.cpp
@@ -0,0 +1,20 @@
+/*********************************************************************
+ ** Author: Devin Gendron
+ ** Date: 9/23/17
+ ** Description:Practicing tools for the course.
+ *********************************************************************/
+
+#include <iostream>
+#include <string>
+
+// a simple example program
+int main()
+{
+    std::string faveAnimal;
+    std::cout << "Please enter your favorite animal." << std::endl;
+    std:: cin >> faveAnimal;
+    std::cout << "You favorite animal is the " << faveAnimal;
+    std::cout << "." << std::endl;
+    
+    return 0;
+}
